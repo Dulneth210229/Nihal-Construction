d02b3e5ba1d0ae5accdb263836c1d15a34425587
Dulneth210229 <dulneth.sa@gmail.com>
2025-02-06 22:00:52 +0530

    final changes

--- script/project.js
+++ script/project.js
@@ -1,3 +1,11 @@
+//mobile toggle
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
 const projectsData = [
   {
     id: 1,
